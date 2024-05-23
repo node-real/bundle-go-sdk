09f980e73598be377d27e551e1df7f844ac34e63
rekyyang <511965710@qq.com>
2024-05-23 15:57:02 +0800

    fix: remove gorm tag

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -17,12 +17,12 @@ const (
 
 type Bundle struct {
 	Hash                 common.Hash
-	Txs                  Hashes         `gorm:"column:txs;type:blob"`
-	MaxBlockNumber       uint64         `gorm:"column:max_block_number"`
-	MaxTimestamp         uint64         `gorm:"column:max_timestamp"`
-	Status               BundleStatus   `gorm:"column:status;default:0"`
-	GasFee               *hexutil.Big   `gorm:"column:gas_fee;type:VARBINARY(32)"`
-	Builder              common.Address `gorm:"column:builder;type:BINARY(20)"`
-	ConfirmedBlockNumber uint64         `gorm:"column:confirmed_block_number"`
-	ConfirmedDate        uint64         `gorm:"column:confirmed_date"`
+	Txs                  Hashes
+	MaxBlockNumber       uint64
+	MaxTimestamp         uint64
+	Status               BundleStatus
+	GasFee               *hexutil.Big
+	Builder              common.Address
+	ConfirmedBlockNumber uint64
+	ConfirmedDate        uint64
 }
