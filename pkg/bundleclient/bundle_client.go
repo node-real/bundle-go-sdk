108cbed2cd8eae9442a52b4174b723b0d5af6bd1
rekyyang <511965710@qq.com>
2024-05-23 16:01:18 +0800

    fix: rename client

--- pkg/bundleclient/bundle_client.go
+++ pkg/bundleclient/bundle_client.go
@@ -11,7 +11,7 @@ import (
 	types2 "github.com/node-real/bundle-go-sdk/pkg/types"
 )
 
-type IClient interface {
+type Client interface {
 	SendBundle(context.Context, types.SendBundleArgs) (common.Hash, error)
 	QueryBundle(context.Context, common.Hash) (*types2.Bundle, error)
 	BundlePrice(context.Context) (*big.Int, error)
@@ -19,15 +19,15 @@ type IClient interface {
 	Validators(context.Context) ([]common.Address, error)
 }
 
-type Client struct {
+type client struct {
 	c *rpc.Client
 }
 
-func New(rpcCli *rpc.Client) *Client {
-	return &Client{rpcCli}
+func New(rpcCli *rpc.Client) Client {
+	return &client{rpcCli}
 }
 
-func (c *Client) SendBundle(ctx context.Context, args types.SendBundleArgs) (common.Hash, error) {
+func (c *client) SendBundle(ctx context.Context, args types.SendBundleArgs) (common.Hash, error) {
 	var hash common.Hash
 	err := c.c.CallContext(ctx, &hash, "eth_sendBundle", args)
 	if err != nil {
@@ -36,7 +36,7 @@ func (c *Client) SendBundle(ctx context.Context, args types.SendBundleArgs) (com
 	return hash, nil
 }
 
-func (c *Client) QueryBundle(ctx context.Context, bundleHash common.Hash) (*types2.Bundle, error) {
+func (c *client) QueryBundle(ctx context.Context, bundleHash common.Hash) (*types2.Bundle, error) {
 	var bundle types2.Bundle
 	err := c.c.CallContext(ctx, &bundle, "eth_queryBundle", bundleHash)
 	if err != nil {
@@ -45,7 +45,7 @@ func (c *Client) QueryBundle(ctx context.Context, bundleHash common.Hash) (*type
 	return &bundle, nil
 }
 
-func (c *Client) BundlePrice(ctx context.Context) (*big.Int, error) {
+func (c *client) BundlePrice(ctx context.Context) (*big.Int, error) {
 	var price *big.Int
 	err := c.c.CallContext(ctx, &price, "eth_bundlePrice")
 	if err != nil {
@@ -54,7 +54,7 @@ func (c *Client) BundlePrice(ctx context.Context) (*big.Int, error) {
 	return price, nil
 }
 
-func (c *Client) Builders(ctx context.Context) ([]common.Address, error) {
+func (c *client) Builders(ctx context.Context) ([]common.Address, error) {
 	var builders []common.Address
 	err := c.c.CallContext(ctx, &builders, "eth_builders")
 	if err != nil {
@@ -63,7 +63,7 @@ func (c *Client) Builders(ctx context.Context) ([]common.Address, error) {
 	return builders, nil
 }
 
-func (c *Client) Validators(ctx context.Context) ([]common.Address, error) {
+func (c *client) Validators(ctx context.Context) ([]common.Address, error) {
 	var validators []common.Address
 	err := c.c.CallContext(ctx, &validators, "eth_validators")
 	if err != nil {
